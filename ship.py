c6b43466cb34e97e6304f00cb676e2d333e1d5ad
Gleb Danshin <glebd79@gmail.com>
2021-09-17 18:15:36 +0300

    ai_game changed to screen in Ship

--- ship.py
+++ ship.py
@@ -1,13 +1,15 @@
 import pygame
 
-class Ship():
-	def __init__(self, ai_game):
-		self.screen = ai_game.screen
-		self.screen_rect = ai_game.screen.get_rect()
 
-		self.image = pygame.image.load('images/ship.bmp')
-		self.rect = self.image.get_rect()
-		self.rect.midbottom = self.screen_rect.midbottom
+class Ship:
 
-	def blitme(self):
-		self.screen.blit(self.image, self.rect)
+    def __init__(self, screen):
+        self.screen = screen
+        self.screen_rect = screen.get_rect()
+
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
